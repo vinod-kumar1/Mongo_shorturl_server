278bc9dad7877cc127bb95d69afa7c3d774c3399
vinod-kumar1 <vinu22149@gmail.com>
2024-09-12 12:26:41 +0530

    tes

--- api/index.js
+++ api/index.js
@@ -1,9 +1,51 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-app.get("/url", (req, res) => res.send("Express on Vercel"));
-app.get("/url/:hash", (req, res) => res.json({name : req.params.hash}));
+// app.post("/geturl", (req, res) => res.send("Express on Vercel"));
+// app.get("/url/:hash", (req, res) => res.json({name : req.params.hash}));
 
-app.listen(4000, () => console.log("Server ready on port 4000."));
+// app.listen(4000, () => console.log("Server ready on port 4000."));
 
-module.exports = app;
+// module.exports = app;
+
+import genHash from "./hashUrl.js";
+import { connectDb } from "./connectDb.js";
+connectDb().catch(console.log);
+import { storeIndb, searchDb } from "./connectDb.js";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+let app = express();
+app.listen(process.env.PORT || 3500);
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+let corsOptions = {
+  origin: "*",
+  allowedHeaders: ["Content-Type"],
+};
+
+app.post("/geturl", cors(corsOptions), async (req, res) => {
+  let hash = await genHash(req.body.url);
+  await storeIndb(hash, req.body.url);
+  res.json({ hash: hash });
+});
+
+app.get("/favicon.ico", (req, res) => res.status(204).end());
+
+app.get("/:hash", async (req, res) => {
+  let search = await searchDb(req.params.hash);
+  try {
+    if (search.search) {
+      res.redirect(search.url);
+      res.end();
+    } else {
+      console.log("404 not found!");
+      res.sendFile("/Users/vinod/Desktop/URL-Shortner/public/404.html");
+    }
+  } catch (err) {
+    console.log("Error", err);
+  }
+});
